6e1a622a2301ef7a43c04e3c1fafdfa27a8ae311
Divyaarghekar <divya.argekar@gmail.com>
2019-06-18 15:15:21 +0530

    constant changes added

--- iSolusV3Framework/src/Utility/Constant.java
+++ iSolusV3Framework/src/Utility/Constant.java
@@ -2,7 +2,7 @@ package Utility;
 
 public class Constant {
 public static final String URL="http://192.168.2.241/isolusv3/#/app/airport";
-public static final String URL2="https://www.highcharts.com/demo/line-ajax";
+public static final String URL2="https://www.highchart.com/demo/line-ajax";
 public static final int Col_TestCaseName = 0; 
 
 public static final int Col_UserName =1 ;
